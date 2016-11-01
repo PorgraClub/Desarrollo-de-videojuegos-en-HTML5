83a534f6ba8edaa5a1d12b6ea90a81ff81e19074
Progra Club <prograclub@gmail.com>
2016-11-01 00:58:42 -0500

    Videotutorial de canvas 01

---
+++ 01/main.js
@@ -0,0 +1,14 @@
+var canvas = document.getElementById("micanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(100,100,50,50);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(150,100,50,50);
+
+ctx.fillStyle = "green";
+ctx.fillRect(100,150,50,50);
+
+ctx.fillStyle = "yellow";
+ctx.fillRect(150,150,50,50);
